075183fc1abddbafa94fb8528ff6da521cc2ddb0
Forbes Lindesay <forbes.lindesay@red-gate.com>
2013-10-11 11:17:59 +0100

    Initial code

---
+++ README.md
@@ -0,0 +1,51 @@
+# knockout-code-mirror
+
+Knockout bindings for the code-mirror editor
+
+[![Build Status](https://travis-ci.org/ForbesLindesay/knockout-code-mirror.png?branch=master)](https://travis-ci.org/ForbesLindesay/knockout-code-mirror)
+[![Dependency Status](https://gemnasium.com/ForbesLindesay/knockout-code-mirror.png)](https://gemnasium.com/ForbesLindesay/knockout-code-mirror)
+[![NPM version](https://badge.fury.io/js/knockout-code-mirror.png)](http://badge.fury.io/js/knockout-code-mirror)
+
+## Installation
+
+    npm install knockout-code-mirror
+
+N.B. This module peer depends on `code-mirror` and `knockout`.  It will fail to install if you are not using the same version of knockout and code-mirror as this module.  You will need to require in whichever themes and modes you want from code-mirror (see [code-mirror documentation](https://github.com/ForbesLindesay/code-mirror))
+
+## Example
+
+To use, simply use the `codemirror` data binding:
+
+```html
+<div data-bind="codemirror: { value: sourceText, mode: 'sql' }"></div>
+```
+
+```javascript
+var ko = require('knockout');
+var enableCM = require('knockout-code-mirror');
+require('code-mirror/mode/sql');
+require('code-mirror/theme/monokai');
+
+//set defaults for the binding
+enableCM({
+  theme: 'monokai'
+}).on('keyup', function (cm) {
+  console.log('keyup event on:');
+  console.dir(cm);
+});
+
+ko.applyBindings({
+  sourceText: 'SELECT * FROM CodeMirrorModes'
+});
+```
+
+## Events
+
+All the builtin code mirror events are proxied for `on` (but not for `off`).  In addition, the following events are fired:
+
+ - `editor-created(editor, parentElement, options)` - fired when a new editor is created
+ - `editor-disposed(editor, parentElement, options)` - fired when an editor is disposed (so you can do cleanup).
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,73 @@
+'use strict';
+
+var ko = require('knockout');
+var CodeMirror = require('code-mirror');
+var themes = require('code-mirror/theme');
+
+module.exports = enableCM;
+function enableCM(defaults) {
+  var events = {
+    'editor-created': [],
+    'editor-disposed': []
+  };
+
+  ko.bindingHandlers.codemirror = {
+    init: function(element, valueAccessor, allBindingsAccessor, viewModel) {
+      var options = ko.toJS(valueAccessor());
+      for (var key in defaults) {
+        if (options[key] === undefined) {
+          options[key] = defaults[key];
+        }
+      }
+      if (options.theme && !themes.available(options.theme) && typeof console === 'object' && console) {
+        console.error(options.theme + ' may not be an available theme.');
+      }
+      options.value = options.value || '';
+      var editor = new CodeMirror(element, options);
+      editor.on('change', function (cm) {
+        var value = ko.unwrap(valueAccessor()).value;
+        if (ko.isObservable(value)) {
+          value(cm.getValue());
+        } else {
+          ko.unwrap(valueAccessor()).value = cm.getValue();
+        }
+      });
+      for (var i = 0; i < events['editor-created'].length; i++) {
+        events['editor-created'][i](editor, element, options);
+      }
+      for (var key in events) {
+        if (key !== 'editor-created' && key !== 'editor-disposed') {
+          for (var i = 0; i < events[key].length; i++) {
+            editor.on(key, events[key][i]);
+          }
+        }
+      }
+
+      var wrapperElement = $(editor.getWrapperElement());
+      ko.utils.domNodeDisposal.addDisposeCallback(element, function() {
+        for (var i = 0; i < events['editor-disposed'].length; i++) {
+          events['editor-disposed'][i](editor, element, options);
+        }
+        wrapperElement.remove();
+      });
+    }
+  };
+
+  return {
+    on: function (name, fn) {
+      events[name] = events[name] || [];
+      if (events[name].indexOf(fn) === -1) {
+        events[name].push(fn);
+      }
+    },
+    off: function (name, fn) {
+      if (name !== 'editor-created' && name !== 'editor-disposed') {
+        throw new Error('`off` is not supported when proxying to CodeMirror events');
+      }
+      events[name] = events[name] || [];
+      events[name] = events[name].filter(function (f) {
+        return f !== fn;
+      })
+    }
+  }
+}
